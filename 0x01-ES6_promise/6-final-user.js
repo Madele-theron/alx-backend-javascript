9334702db6d677d3a2fdb037614f98e97a9470ec
Madele <madele.theron@gmail.com>
2023-04-19 22:34:22 +0200

    task 6: add output array & status rejected

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -5,5 +5,22 @@ export default function handleProfileSignup(firstName, lastName, fileName) {
   const userPromise = signUpUser(firstName, lastName);
   const photoPromise = uploadPhoto(fileName);
 
-  return Promise.allSettled([userPromise, photoPromise]);
+  return Promise.allSettled([userPromise, photoPromise])
+    .then((results) => {
+      const output = [];
+      for (const result in results) {
+        if (result === 'fulfilled') {
+          output.push({
+            status: 'fulfilled',
+            value: result.value,
+          });
+        } else {
+          output.push({
+            status: 'rejected',
+            value: result.reason ? result.reason.toString() : 'Unknown error',
+          });
+        }
+      }
+      return output;
+    });
 }
