ba51520cc060a9ad184350a65e144426b892aedf
Madele <madele.theron@gmail.com>
2023-04-20 21:41:21 +0200

    task 8

---
+++ 0x02-ES6_classes/8-hbtn_class.js
@@ -0,0 +1,16 @@
+export default class HolbertonClass {
+  constructor(size, location) {
+    this._size = size;
+    this._location = location;
+  }
+
+  //  cast to a number
+  valueOf() {
+    return this._size;
+  }
+
+  //   cast to a string
+  toString() {
+    return this._locaiton;
+  }
+}
