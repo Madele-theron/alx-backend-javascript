f7c4c5415c577b73f39b37bf0c1d9151a25987c0
Madele <madele.theron@gmail.com>
2023-04-25 23:39:31 +0200

    removed second map.set-it was replacing new values

--- 0x03-ES6_data_manipulation/10-update_uniq_items.js
+++ 0x03-ES6_data_manipulation/10-update_uniq_items.js
@@ -5,9 +5,8 @@ export default function updateUniqueItems(map) {
 
   map.forEach((value, key) => {
     if (value === 1) {
-      map.set(key, value = 100);
+      map.set(key, 100);
     }
-    map.set(key, value);
   });
 
   return map;
