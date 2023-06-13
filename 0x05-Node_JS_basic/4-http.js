51e7bdc6cf76e301c69a43e4b17639d3329be8df
Madele Theron <59708219+Madele-theron@users.noreply.github.com>
2023-06-13 22:59:21 +0200

    task 4

---
+++ 0x05-Node_JS_basic/4-http.js
@@ -0,0 +1,15 @@
+const http = require('http');
+
+const PORT = 1245;
+const HOST = 'localhost';
+
+const app = http.createServer((request, response) => {
+  response.statusCode = 200;
+  response.setHeader('Content-Type', 'text/plain');
+  response.end('Hello Holberton School!');
+});
+
+app.listen(PORT, HOST, () => {
+});
+
+module.exports = app;
