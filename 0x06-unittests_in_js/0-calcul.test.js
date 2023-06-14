649c1398bab2b99cfca3519b1e6566b2d67faf4d
Madele Theron <59708219+Madele-theron@users.noreply.github.com>
2023-06-14 21:39:26 +0200

    back to normal

--- 0x06-unittests_in_js/0-calcul.test.js
+++ 0x06-unittests_in_js/0-calcul.test.js
@@ -2,16 +2,10 @@ const calculateNumber = require('./0-calcul')
 const assert = require('assert');
 
 describe('calculateNumber', function () {
-      it('test function with values', function () {
+    it('test with values', function () {
         assert.strictEqual(calculateNumber(1, 3), 4);
         assert.strictEqual(calculateNumber(1, 3.7), 5);
-        assert.strictEqual(calculateNumber(3.7, 1), 5);
-        assert.strictEqual(calculateNumber(1, 3.3), 4);
-        assert.strictEqual(calculateNumber(3.3, 1), 4);
         assert.strictEqual(calculateNumber(1.2, 3.7), 5);
-        assert.strictEqual(calculateNumber(3.7, 1.2), 5);
-        assert.strictEqual(calculateNumber(1.5, 3.7), 6)
-        assert.strictEqual(calculateNumber(3.7, 1.2), 5);
-        assert.strictEqual(calculateNumber(1.2, 2.1), 3);
-      });
-  })
+        assert.strictEqual(calculateNumber(1.5, 3.7), 6);
+    });
+});
