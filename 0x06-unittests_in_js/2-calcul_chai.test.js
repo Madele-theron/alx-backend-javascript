a9a2e06c12796e28bb9cee297c2397232ec10fde
Madele Theron <59708219+Madele-theron@users.noreply.github.com>
2023-06-14 23:14:29 +0200

    test caese task v2

--- 0x06-unittests_in_js/2-calcul_chai.test.js
+++ 0x06-unittests_in_js/2-calcul_chai.test.js
@@ -5,8 +5,8 @@ const assert = require('assert').strict;
 describe('calculateNumber type == SUM', () => {
     it('checks neg & pos', () => {
         assert.strictEqual(calculateNumber('SUM', 1, 3), 4);
-        assert.strictEqual(calculateNumber('SUM', 1.9, 0), 2);
-        assert.strictEqual(calculateNumber('SUM', 6.1, 6.1), 12);
+        assert.strictEqual(calculateNumber('SUM', 1.6, 2.6), 5);
+        assert.strictEqual(calculateNumber('SUM', 1, 2), 3);
         assert.strictEqual(calculateNumber('SUM', 0.1, 0.2), 0);
         assert.strictEqual(calculateNumber('SUM', 1.4, 4.5), 6);
         assert.strictEqual(calculateNumber('SUM', -1, -3), -4);
@@ -17,17 +17,19 @@ describe('calculateNumber type == SUM', () => {
 describe('calculateNumber type == SUBTRACT', () => {
     it('checks neg & pos', () => {
         assert.strictEqual(calculateNumber('SUBTRACT', -1.4, -3.6), 3);
-        assert.strictEqual(calculateNumber('SUBTRACT', 1.9, 0), 2);
+        assert.strictEqual(calculateNumber('SUBTRACT', 1.4, 2.3), -1);
         assert.strictEqual(calculateNumber('SUBTRACT', -1, -3), 2);
         assert.strictEqual(calculateNumber('SUBTRACT', 6.1, 6.1), 0);
-        assert.strictEqual(calculateNumber('SUBTRACT', -1.5, 0), -1);
-        assert.strictEqual(calculateNumber('SUBTRACT', 4.5, 2), 3);
+        assert.strictEqual(calculateNumber('SUBTRACT', -4.9, -2.7), -2);
+        assert.strictEqual(calculateNumber('SUBTRACT', 4.9, 2.7), 2);
     });
 });
 
 describe('calculateNumber type == DIVIDE', () => {
 it('checks neg & pos', () => {
+    assert.strictEqual(calculateNumber('DIVIDE', 4, 2), 2);
     assert.strictEqual(calculateNumber('DIVIDE', -1, 1), -1);
+    assert.strictEqual(calculateNumber('DIVIDE', 4.6, 1.8), 2.5);
     assert.strictEqual(calculateNumber('DIVIDE', 4, 0), 'Error');
 });
 });
