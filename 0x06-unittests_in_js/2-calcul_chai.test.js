1232e421cdf83ff805c372213fedf790013d5d19
Madele Theron <59708219+Madele-theron@users.noreply.github.com>
2023-06-14 23:43:49 +0200

    task 2 final

--- 0x06-unittests_in_js/2-calcul_chai.test.js
+++ 0x06-unittests_in_js/2-calcul_chai.test.js
@@ -1,37 +1,33 @@
-const calculateNumber = require('./2-calcul_chai')
-const { expect } = require('chai');
-const assert = require('assert').strict;
+const expect = require("chai").expect;
+const {describe, it} = require("mocha");
+const calculateNumber = require("./2-calcul_chai");
 
 describe("calculateNumber", function() {
     describe('SUM', () => {
         it('checks neg & pos', () => {
-            assert.strictEqual(calculateNumber('SUM', 1, 3), 4);
-            assert.strictEqual(calculateNumber('SUM', 1.6, 2.6), 5);
-            assert.strictEqual(calculateNumber('SUM', 1, 2), 3);
-            assert.strictEqual(calculateNumber('SUM', 0.1, 0.2), 0);
-            assert.strictEqual(calculateNumber('SUM', 1.4, 4.5), 6);
-            assert.strictEqual(calculateNumber('SUM', -1, -3), -4);
-            assert.strictEqual(calculateNumber('SUM', 3.7, 1), 5);
+            expect(calculateNumber('SUM', 1, 3)).to.equal(4);
+            expect(calculateNumber('SUM', 1.6, 2.6)).to.equal(5);
+            expect(calculateNumber('SUM', 1, 2)).to.equal(3);
         })
     });
 
     describe('SUBTRACT', () => {
         it('checks neg & pos', () => {
-            assert.strictEqual(calculateNumber('SUBTRACT', -1.4, -3.6), 3);
-            assert.strictEqual(calculateNumber('SUBTRACT', 1.4, 2.3), -1);
-            assert.strictEqual(calculateNumber('SUBTRACT', -1, -3), 2);
-            assert.strictEqual(calculateNumber('SUBTRACT', 6.1, 6.1), 0);
-            assert.strictEqual(calculateNumber('SUBTRACT', -4.9, -2.7), -2);
-            assert.strictEqual(calculateNumber('SUBTRACT', 4.9, 2.7), 2);
+            expect(calculateNumber('SUBTRACT', -1.4, -3.6)).to.equal(3);
+            expect(calculateNumber('SUBTRACT', 1.4, 2.3)).to.equal(-1);
+            expect(calculateNumber('SUBTRACT', -1, -3)).to.equal(2);
+            expect(calculateNumber('SUBTRACT', 6.1, 6.1)).to.equal(0);
+            expect(calculateNumber('SUBTRACT', -4.9, -2.7)).to.equal(-2);
+            expect(calculateNumber('SUBTRACT', 4.9, 2.7)).to.equal(2);
         });
     });
 
     describe('DIVIDE', () => {
     it('checks neg & pos', () => {
-        assert.strictEqual(calculateNumber('DIVIDE', 4, 2), 2);
-        assert.strictEqual(calculateNumber('DIVIDE', -1, 1), -1);
-        assert.strictEqual(calculateNumber('DIVIDE', 4.6, 1.8), 2.5);
-        assert.strictEqual(calculateNumber('DIVIDE', 4, 0), 'Error');
+        expect(calculateNumber('DIVIDE', 4, 2)).to.equal(2);
+        expect(calculateNumber('DIVIDE', -1, 1)).to.equal(-1);
+        expect(calculateNumber('DIVIDE', 4.6, 1.8)).to.equal(2.5);
+        expect(calculateNumber('DIVIDE', 4, 0)).to.equal('Error');
     });
     });
 });
