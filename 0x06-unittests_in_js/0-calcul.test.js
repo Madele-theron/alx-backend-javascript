d9bc26bd98e871430a3fba5feda7a0ba637d8c71
Madele Theron <59708219+Madele-theron@users.noreply.github.com>
2023-06-14 22:05:05 +0200

    update task 0 tests

--- 0x06-unittests_in_js/0-calcul.test.js
+++ 0x06-unittests_in_js/0-calcul.test.js
@@ -1,7 +1,9 @@
 const calculateNumber = require('./0-calcul')
+const { it, describe } = require("mocha")
 const assert = require('assert');
 
 describe('calculateNumber', function () {
+    
     it('test with values', function () {
         assert.strictEqual(calculateNumber(1, 3), 4);
         assert.strictEqual(calculateNumber(1, 3.7), 5);
