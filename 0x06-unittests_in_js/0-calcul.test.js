b5b0f0792dfa1149ebdcae8734fff66c384971d8
Madele Theron <59708219+Madele-theron@users.noreply.github.com>
2023-06-14 23:02:33 +0200

    fix test result

--- 0x06-unittests_in_js/0-calcul.test.js
+++ 0x06-unittests_in_js/0-calcul.test.js
@@ -9,7 +9,7 @@ describe('calculateNumber()', function () {
         assert.strictEqual(calculateNumber(1, 3), 4);
         assert.strictEqual(calculateNumber(1, 3.7), 5);
         assert.strictEqual(calculateNumber(1.2, 3.7), 5);
-        assert.strictEqual(calculateNumber(1.5, 3.7), 5);
+        assert.strictEqual(calculateNumber(1.5, 3.7), 6);
         assert.strictEqual(calculateNumber(-1.6, -1.7), -4);
     });
 });
