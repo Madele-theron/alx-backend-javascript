954f3187a460915bf468c44053fb53148aa1a7b7
Madele Theron <59708219+Madele-theron@users.noreply.github.com>
2023-06-14 22:37:47 +0200

    correct output

--- 0x06-unittests_in_js/0-calcul.test.js
+++ 0x06-unittests_in_js/0-calcul.test.js
@@ -9,6 +9,6 @@ describe('calculateNumber()', function () {
         assert.strictEqual(calculateNumber(1, 3), 4);
         assert.strictEqual(calculateNumber(1, 3.7), 5);
         assert.strictEqual(calculateNumber(1.2, 3.7), 5);
-        assert.strictEqual(calculateNumber(1.5, 3.7), 6);
+        assert.strictEqual(calculateNumber(1.5, 3.7), 5);
     });
 });
