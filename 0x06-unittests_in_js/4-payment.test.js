8dc5557e5c93fba35d0dbc8ab95d54b35d9c8868
Madele Theron <59708219+Madele-theron@users.noreply.github.com>
2023-06-15 00:10:58 +0200

    tsk 4

---
+++ 0x06-unittests_in_js/4-payment.test.js
@@ -0,0 +1,17 @@
+const sendPaymentRequestToApi = require("./4-payment");
+const Utils = require("./utils");
+const {describe, it} = require("mocha");
+const sinon = require("sinon");
+const assert = require("assert");
+
+describe("sendPaymentRequestToApi", () => {
+    it("stub Utils.calculateNumber", () =>  {
+    const spy = sinon.spy(console, "log");
+	const stub = sinon.spy(Utils, "calculateNumber").returns(10);
+
+    sendPaymentRequestToApi(100, 20);
+
+	assert(spy.withArgs("The total is: 10").calledOnce);
+	assert(stub.withArgs("SUM", 100, 20).calledOnce);
+    });
+});
