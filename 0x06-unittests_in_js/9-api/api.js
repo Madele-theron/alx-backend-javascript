75290c6bd6967cdf5f9e08257d413b5b061ed928
Madele Theron <59708219+Madele-theron@users.noreply.github.com>
2023-06-16 11:03:51 +0200

    Update tsk 9 api.js

--- 0x06-unittests_in_js/9-api/api.js
+++ 0x06-unittests_in_js/9-api/api.js
@@ -4,6 +4,7 @@ const app = express();
 const PORT = 7865;
 
 app.get('/', (request, response) => response.end('Welcome to the payment system'))
+app.get('/cart/:id([0-9]+)', (request, response) => response.send(`Payment methods for cart ${request.params.id}`))
 app.listen(PORT, () => console.log(`API available on localhost port ${PORT}`));
 
 module.exports = app;
