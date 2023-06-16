6a600aa6a8801b226ac3a154371f8d8802cb156f
Madele Theron <59708219+Madele-theron@users.noreply.github.com>
2023-06-16 21:41:22 +0200

    fix import in index.js

--- 0x05-Node_JS_basic/full_server/server.js
+++ 0x05-Node_JS_basic/full_server/server.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const controllingRouter = require('./routes/index.js');
+const controllingRouter = require('./controllingRouter/index.js');
 const app = express();
 const PORT = 1245;
 
