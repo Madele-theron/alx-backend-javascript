338e19c0c1384ae8092d9aa24731d64a20b217e0
Madele Theron <59708219+Madele-theron@users.noreply.github.com>
2023-06-16 17:44:59 +0200

    lets see

--- 0x05-Node_JS_basic/3-read_file_async.js
+++ 0x05-Node_JS_basic/3-read_file_async.js
@@ -1,37 +1,38 @@
+'use strict';
+
 const fs = require('fs');
 
 const countStudents = (fileName) => new Promise((resolve, reject) => {
   fs.readFile(fileName, (error, data) => {
     if (error) {
-      reject(new Error('Cannot load the database'));
+      reject(new Error(`Cannot load the database: ${error.message}`));
     }
     if (data) {
-      const fileLines = data
-        .toString('utf-8')
-        .split('\n');
+      const fileLines = data.toString('utf-8').split('\n');
       const GroupsObj = {};
-      const FieldNames = fileLines[0].split(',');
-      const PropNames = FieldNames
-        .slice(0, FieldNames.length - 1);
-      console.log(fileLines.slice(0))
-      for (const line of fileLines.slice()) {
+      const [firstLine, ...lines] = fileLines;
+      const FieldNames = firstLine.split(',');
+      const PropNames = FieldNames.slice(0, FieldNames.length - 1);
+
+      for (const line of lines) {
         const studentRecord = line.split(',');
-        const studentPropValues = studentRecord
-          .slice(0, studentRecord.length - 1);
+        const studentPropValues = studentRecord.slice(0, studentRecord.length - 1);
         const field = studentRecord[studentRecord.length - 1];
-        if (!Object.keys(GroupsObj).includes(field)) {
+        
+        if (!GroupsObj.hasOwnProperty(field)) {
           GroupsObj[field] = [];
         }
-        const studentEntries = PropNames
-          .map((propName, idx) => [propName, studentPropValues[idx]]);
+        
+        const studentEntries = PropNames.map((propName, idx) => [propName, studentPropValues[idx]]);
         GroupsObj[field].push(Object.fromEntries(studentEntries));
       }
 
-      console.log(`Number of students: 10`);
+      console.log(`Number of students: ${lines.length}`);
       for (const [field, group] of Object.entries(GroupsObj)) {
-        const studentNames = group.map((student) => student.firstname).join(', ');
+        const studentNames = group.map(({ firstname }) => firstname).join(', ');
         console.log(`Number of students in ${field}: ${group.length}. List: ${studentNames}`);
       }
+
       resolve(true);
     }
   });
