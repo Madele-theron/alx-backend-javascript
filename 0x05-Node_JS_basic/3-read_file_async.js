1a0fea82546367983c5e60423dbf4caf4601ac96
Madele Theron <59708219+Madele-theron@users.noreply.github.com>
2023-06-16 17:53:32 +0200

    try my old code
    see if it fixe the eslint issue

--- 0x05-Node_JS_basic/3-read_file_async.js
+++ 0x05-Node_JS_basic/3-read_file_async.js
@@ -1,39 +1,40 @@
-'use strict';
-
 const fs = require('fs');
 
-const countStudents = (fileName) => new Promise((resolve, reject) => {
-  fs.readFile(fileName, (error, data) => {
+const countStudents = (FileName) => new Promise((resolve, reject) => {
+  fs.readFile(FileName, (error, data) => {
     if (error) {
-      reject(new Error(`Cannot load the database: ${error.message}`));
+      reject(new Error('Cannot load the database'));
     }
     if (data) {
-      const fileLines = data.toString('utf-8').split('\n');
-      const GroupsObj = {};
-      const [firstLine, ...lines] = fileLines;
-      const FieldNames = firstLine.split(',');
-      const PropNames = FieldNames.slice(0, FieldNames.length - 1);
+      const fileLines = data
+        .toString('utf-8')
+        .split('\n');
+      const Groups = {};
+      const FieldNames = fileLines[0].split(',');
+      const PropNames = FieldNames
+        .slice(0, FieldNames.length - 1);
 
-      for (const line of lines) {
+      for (const line of fileLines.slice(1)) {
         const studentRecord = line.split(',');
-        const studentPropValues = studentRecord.slice(0, studentRecord.length - 1);
+        const studentPropValues = studentRecord
+          .slice(0, studentRecord.length - 1);
         const field = studentRecord[studentRecord.length - 1];
-        
-        if (!GroupsObj.hasOwnProperty(field)) {
-          GroupsObj[field] = [];
+        if (!Object.keys(Groups).includes(field)) {
+          Groups[field] = [];
         }
-        
-        const studentEntries = PropNames.map((propName, idx) => [propName, studentPropValues[idx]]);
-        GroupsObj[field].push(Object.fromEntries(studentEntries));
+        const studentEntries = PropNames
+          .map((propName, idx) => [propName, studentPropValues[idx]]);
+        Groups[field].push(Object.fromEntries(studentEntries));
       }
 
-      console.log(`Number of students: 10`);
-      console.log('Number of students in field: 1. List: firstname')
-      for (const [field, group] of Object.entries(GroupsObj)) {
-        const studentNames = group.map(({ firstname }) => firstname).join(', ');
+      const totalStudents = Object
+        .values(Groups)
+        .reduce((previous, current) => (previous || []).length + current.length);
+      console.log(`Number of students: ${totalStudents}`);
+      for (const [field, group] of Object.entries(Groups)) {
+        const studentNames = group.map((student) => student.firstname).join(', ');
         console.log(`Number of students in ${field}: ${group.length}. List: ${studentNames}`);
       }
-
       resolve(true);
     }
   });
