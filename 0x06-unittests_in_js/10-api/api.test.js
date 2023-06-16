7b3712c507fbebdf0287a2dd50871d3d5a7028a3
Madele Theron <59708219+Madele-theron@users.noreply.github.com>
2023-06-16 11:18:05 +0200

    task 10 update api.test

--- 0x06-unittests_in_js/10-api/api.test.js
+++ 0x06-unittests_in_js/10-api/api.test.js
@@ -17,31 +17,31 @@ describe('GET /', () => {
     });
 });
 
-    describe('GET /cart', () => {
-        it('endpoint: GET /cart', (done) => {
-            const apicall = {
-            url: 'http://localhost:7865/cart/12',
-            method: 'GET',
-            };
+describe('GET /cart', () => {
+    it('endpoint: GET /cart', (done) => {
+        const apicall = {
+        url: 'http://localhost:7865/cart/12',
+        method: 'GET',
+        };
 
-            request(apicall, (error, response, body) => {
-                chai.expect(response.statusCode).to.equal(200);
-                chai.expect(body).to.equal('Payment methods for cart 12');
-                done();
-            });
+        request(apicall, (error, response, body) => {
+            chai.expect(response.statusCode).to.equal(200);
+            chai.expect(body).to.equal('Payment methods for cart 12');
+            done();
         });
-        describe('GET /cart/:isNaN', () => {
-            it('endpoint: GET /cart/:isNaN', (done) => {
-                const apicall = {
-                url: 'http://localhost:7865/cart/something',
-                method: 'GET',
-                };
-    
-                request(apicall, (error, response, body) => {
-                    chai.expect(response.statusCode).to.equal(404);
-                    done();
-                });
-            });
+    });
+describe('GET /cart/:isNaN', () => {
+    it('endpoint: GET /cart/:isNaN', (done) => {
+        const apicall = {
+        url: 'http://localhost:7865/cart/something',
+        method: 'GET',
+        };
+
+        request(apicall, (error, response, body) => {
+            chai.expect(response.statusCode).to.equal(404);
+            done();
         });
+    });
+});
     });
 });
