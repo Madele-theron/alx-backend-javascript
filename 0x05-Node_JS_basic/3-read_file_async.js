85f357fd760ce7ca6980e28ade735154aec95dc6
Madele Theron <59708219+Madele-theron@users.noreply.github.com>
2023-06-16 12:58:54 +0200

    backn to normail

--- 0x05-Node_JS_basic/3-read_file_async.js
+++ 0x05-Node_JS_basic/3-read_file_async.js
@@ -1,7 +1,7 @@
 const fs = require('fs');
 
-const countStudents = (fileName) => new Promise((resolve, reject) => {
-  fs.readFile(fileName, (error, data) => {
+const countStudents = (FileName) => new Promise((resolve, reject) => {
+  fs.readFile(FileName, (error, data) => {
     if (error) {
       reject(new Error('Cannot load the database'));
     }
@@ -9,8 +9,7 @@ const countStudents = (fileName) => new Promise((resolve, reject) => {
       const fileLines = data
         .toString('utf-8')
         .split('\n');
-      console.log(fileLines)
-      const GroupsObj = {};
+      const Groups = {};
       const FieldNames = fileLines[0].split(',');
       const PropNames = FieldNames
         .slice(0, FieldNames.length - 1);
@@ -20,23 +19,23 @@ const countStudents = (fileName) => new Promise((resolve, reject) => {
         const studentPropValues = studentRecord
           .slice(0, studentRecord.length - 1);
         const field = studentRecord[studentRecord.length - 1];
-        if (!Object.keys(GroupsObj).includes(field)) {
-          GroupsObj[field] = [];
+        if (!Object.keys(Groups).includes(field)) {
+          Groups[field] = [];
         }
         const studentEntries = PropNames
           .map((propName, idx) => [propName, studentPropValues[idx]]);
-          GroupsObj[field].push(Object.fromEntries(studentEntries));
+        Groups[field].push(Object.fromEntries(studentEntries));
       }
 
       const totalStudents = Object
-        .values(GroupsObj)
+        .values(Groups)
         .reduce((previous, current) => (previous || []).length + current.length);
       console.log(`Number of students: ${totalStudents}`);
       for (const [field, group] of Object.entries(Groups)) {
         const studentNames = group.map((student) => student.firstname).join(', ');
         console.log(`Number of students in ${field}: ${group.length}. List: ${studentNames}`);
       }
-      resolve(data);
+      resolve(true);
     }
   });
 });
