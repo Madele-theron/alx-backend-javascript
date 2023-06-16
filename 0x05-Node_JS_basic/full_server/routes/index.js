9f5a8faf261de72185b12e93e5be5f6410098940
Madele Theron <59708219+Madele-theron@users.noreply.github.com>
2023-06-16 21:29:32 +0200

    fix index task 8

--- 0x05-Node_JS_basic/full_server/routes/index.js
+++ 0x05-Node_JS_basic/full_server/routes/index.js
@@ -2,13 +2,12 @@ const express = require('express');
 const AppController = require('../controllers/AppController');
 const StudentsController = require('../controllers/StudentsController');
 
-const controllingRouter = (app) => {
-    const router = express.Router();
-    app.use('/', router);
+const controllingRouter = express.Router();
 
-    router.get('/', AppController.getHomepage);
-    router.get('/students', StudentsController.getAllStudents);
-    router.get('/students/:major', StudentsController.getAllStudentsByMajor);
-}
-// hii
+router.get('/', AppController.getHomepage);
+
+router.get('/students', StudentsController.getAllStudents);
+
+router.get('/students/:major', StudentsController.getAllStudentsByMajor);
+
 module.exports = controllingRouter;
